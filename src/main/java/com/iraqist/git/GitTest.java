0026b3b0cc45c90cd26a427e25068e4a30c79603
Iraqist <Iraqist@163.com>
2022-02-26 20:31:22 +0800

    first commit

---
+++ src/main/java/com/iraqist/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.iraqist.git;/*
+@author Liqing
+@create 2022-02-26-20:13
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
